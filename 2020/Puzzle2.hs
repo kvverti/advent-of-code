ec7203729dd7826569af26f8c3662f3935268b00
Thalia Nero <chrisvn00@gmail.com>
2020-12-02 01:30:18 -0500

    Puzzle 2 part 2

--- 2020/Puzzle2.hs
+++ 2020/Puzzle2.hs
@@ -1,6 +1,7 @@
 -- Thalia Nero, Advent of Code 2020
 module Puzzle2 where
 
+import Data.Maybe
 import Data.List
 
 data PassPolicy = PassPolicy Int Int Char
@@ -16,22 +17,40 @@ test1Text = "1-3 a: abcde\n\
             \1-3 b: cdefg\n\
             \2-9 c: ccccccccc"
 
--- Determines whether a password fulfills the given PassPolicy.
-valid :: PassPolicy -> String -> Bool
-valid (PassPolicy least most ch) pass =
+-- Determines whether a password fulfills the given PassPolicy, part 1.
+valid1 :: (PassPolicy, String) -> Bool
+valid1 (PassPolicy least most ch, pass) =
     let ntimes = length $ filter (== ch) pass in
         ntimes >= least && ntimes <= most
+
+-- Determines whether a password fulfills the given PassPolicy, part 2.
+valid2 :: (PassPolicy, String) -> Bool
+valid2 (PassPolicy pos1 pos2 ch, pass) =
+    let passLs = zip [1..] pass
+        charEq pos = fromMaybe False $ (== ch) <$> lookup pos passLs in
+        charEq pos1 /= charEq pos2
 
 -- Parses an input string into structured data.
 input :: String -> [(PassPolicy, String)]
 input s = policy <$> words . (\\ ":") <$> lines s
     where policy :: [String] -> (PassPolicy, String)
           policy [bs, [ch], ps] = let (least, most') = break (== '-') bs in
-                                    (PassPolicy (read least) (read . tail $ most') ch, ps)
-                                    
-part1 :: [(PassPolicy, String)] -> Int
-part1 ps = length $ filter id $ map (uncurry valid) ps
+                                      (PassPolicy (read least) (read . tail $ most') ch, ps)
+
+-- Switch on which part.
+part :: Int -> (PassPolicy, String) -> Bool
+part 1 = valid1
+part 2 = valid2
+part _ = const False
+
+-- Compute the solution based on the part.
+solution :: Int -> [(PassPolicy, String)] -> Int
+solution which = length . filter id . map (part which)
 
 main :: IO ()
-main = putStrLn =<< show . part1 . input <$> readFile "input2.txt"
+main = do
+    putStr "Part: "
+    part <- readLn
+    xs <- input <$> readFile "input2.txt"
+    putStrLn $ show . solution part $ xs
 
