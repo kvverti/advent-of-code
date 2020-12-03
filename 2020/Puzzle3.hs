2038b4d4dd0e25df03ed42fc394fdc5aefd84f59
Thalia Nero <chrisvn00@gmail.com>
2020-12-03 01:43:43 -0500

    Puzzle 3 part 1

---
+++ 2020/Puzzle3.hs
@@ -0,0 +1,62 @@
+-- Thalia Nero, Advent of Code 2020
+module Puzzle3 where
+
+import Control.Monad.Trans.State
+
+-- Forest of width, trees
+data Forest = Forest Int [Foliage]
+    deriving (Eq, Show)
+
+-- A possible foliage state.
+data Foliage = Full | Clear
+    deriving (Eq, Show)
+
+-- Returns how significant the foliage represented.
+density :: Foliage -> Integer
+density Full = 1
+density Clear = 0
+
+text1 :: String
+text1 = "..##.......\n\
+        \#...#...#..\n\
+        \.#....#..#.\n\
+        \..#.#...#.#\n\
+        \.#...##..#.\n\
+        \..#.##.....\n\
+        \.#.#.#....#\n\
+        \.#........#\n\
+        \#.##...#...\n\
+        \#...##....#\n\
+        \.#..#...#.#"
+
+-- Converts an input string grid to a Forest.
+input :: String -> Forest
+input s = let trees = map disc <$> lines s in
+              Forest (length $ head trees) (concat trees)
+    where disc :: Char -> Foliage
+          disc '#' = Full
+          disc '.' = Clear
+
+-- Evaluates a Forest path using the given (fall, run).
+nextPoint :: (Int, Int) -> (Int, Forest) -> (Int, Forest)
+nextPoint (fall, run) (x, Forest w ts) = let fall' = fall * w
+                                             run' = (x + run) `mod` w
+                                             ts' = drop fall' ts in
+                                             (run', Forest w ts')
+
+-- Given the step, compute the foliage encountered along the path through the forest.
+path :: (Int, Int) -> Forest -> [Foliage]
+path step forest = let next = nextPoint step in
+                       map toTree $ takeWhile unfinished $ iterate next (0, forest)
+    where toTree :: (Int, Forest) -> Foliage
+          toTree (x, Forest _ ts) = ts !! x
+          unfinished :: (Int, Forest) -> Bool
+          unfinished (x, Forest _ ts) = drop x ts /= []
+
+-- Solves part 1 of the puzzle.
+part1 :: Forest -> Integer
+part1 forest = sum $ density <$> path (1, 3) forest
+
+main :: IO ()
+main = putStrLn =<< show . part1 . input <$> readFile "input3.txt"
+                                          
