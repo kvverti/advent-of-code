e2e1976d269ffa4582dc02634e492b3a53722c8d
Thalia Nero <chrisvn00@gmail.com>
2020-12-04 01:56:26 -0500

    Puzzle 4 part 2

--- 2020/Puzzle4.hs
+++ 2020/Puzzle4.hs
@@ -1,26 +1,21 @@
 -- Thalia Nero, Advent of Code 2020
 module Puzzle4 where
 
+import Data.Ix
 import Data.List
 import Data.Maybe
-import Data.Traversable
 
 type Year = Integer
 
-data Passport = Passport { ibr :: Year
+-- A passport.
+data Passport = Passport { byr :: Year
                          , iyr :: Year
                          , eyr :: Year
-                         , hgt :: Integer
+                         , hgt :: String
                          , hcl :: String
                          , ecl :: String
-                         , pid :: Integer
-                         , cid :: Integer }
-                         
-type PassportDict = [(String, String)]
-
--- The required keys for a passport.
-keys :: [String]
-keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"] -- no cid >:)
+                         , pid :: String }
+    deriving (Eq, Show)
 
 text1 :: String
 text1 = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n\
@@ -37,6 +32,68 @@ text1 = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n\
         \hcl:#cfa07d eyr:2025 pid:166559648\n\
         \iyr:2011 ecl:brn hgt:59in"
 
+invalid1 :: String
+invalid1 = "eyr:1972 cid:100\n\
+           \hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n\
+           \\n\
+           \iyr:2019\n\
+           \hcl:#602927 eyr:1967 hgt:170cm\n\
+           \ecl:grn pid:012533040 byr:1946\n\
+           \\n\
+           \hcl:dab227 iyr:2012\n\
+           \ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n\
+           \\n\
+           \hgt:59cm ecl:zzz\n\
+           \eyr:2038 hcl:74454a iyr:2023\n\
+           \pid:3556412378 byr:2007"
+
+valid1 :: String
+valid1 = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\n\
+         \hcl:#623a2f\n\
+         \\n\
+         \eyr:2029 ecl:blu cid:129 byr:1989\n\
+         \iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n\
+         \\n\
+         \hcl:#888785\n\
+         \hgt:164cm byr:2001 iyr:2015 cid:88\n\
+         \pid:545766238 ecl:hzl\n\
+         \eyr:2022\n\
+         \\n\
+         \iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+
+-- Determines whether a passport is "strictly valid"
+strictlyValid :: Passport -> Bool
+strictlyValid (Passport byr iyr eyr hgt hcl ecl pid) =
+    inRange (1920, 2002) byr &&
+    inRange (2010, 2020) iyr &&
+    inRange (2020, 2030) eyr &&
+    height hgt &&
+    hair hcl &&
+    eye ecl &&
+    passId pid
+    where height :: String -> Bool
+          height hgt = let (v', unit) = span (inRange ('0', '9')) hgt
+                           v = read v' in
+                           case unit of
+                                "cm" -> inRange (150, 193) v
+                                "in" -> inRange (59, 76) v
+                                _ -> False
+          hair :: String -> Bool
+          hair hcl = let (prefix, col) = splitAt 1 hcl in
+                         prefix == "#" &&
+                         length col == 6 &&
+                         all (\c -> inRange ('0', '9') c || inRange ('a', 'f') c) col
+          eye :: String -> Bool
+          eye ecl = elem ecl ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+          passId :: String -> Bool
+          passId pid = length pid == 9 && all (inRange ('0', '9')) pid
+
+type PassportDict = [(String, String)]
+
+-- The required keys for a passport.
+keys :: [String]
+keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"] -- no cid >:)
+
 -- Splits input string into passport data segments.
 splitEntries :: String -> [String]
 splitEntries src = unfoldr splitter $ lines src
@@ -44,7 +101,7 @@ splitEntries src = unfoldr splitter $ lines src
           splitter src = case break (== "") src of
                               ([], _) -> Nothing
                               (lns, src') -> Just (intercalate " " lns, dropWhile (== "") src')
-                              
+
 -- Splits input string into passport data.
 passportDict :: String -> PassportDict
 passportDict s = keyVal <$> unfoldr splitter s
@@ -60,13 +117,32 @@ passportDict s = keyVal <$> unfoldr splitter s
 input :: String -> [PassportDict]
 input s = passportDict <$> splitEntries s
 
--- Determines whether a passport dict contains all the valid keys
-validPassport :: PassportDict -> Bool
-validPassport p = isJust . sequence $ flip lookup p <$> keys
+passport :: PassportDict -> Maybe Passport
+passport pd = do
+    byr <- read <$> lookup "byr" pd
+    iyr <- read <$> lookup "iyr" pd
+    eyr <- read <$> lookup "eyr" pd
+    hgt <- lookup "hgt" pd
+    hcl <- lookup "hcl" pd
+    ecl <- lookup "ecl" pd
+    pid <- lookup "pid" pd
+    return $ Passport byr iyr eyr hgt hcl ecl pid
 
 -- Solves part 1 of the puzzle.
 part1 :: [PassportDict] -> Int
-part1 passes = length . filter id $ validPassport <$> passes
+part1 passes = length . catMaybes $ passport <$> passes
+
+-- Solves part 2 of the puzzle.
+part2 :: [PassportDict] -> Int
+part2 passes = length . filter strictlyValid . catMaybes $ passport <$> passes
 
 main :: IO ()
-main = putStrLn =<< show . part1 . input <$> readFile "input4.txt"
+main = do
+    putStr "Part: "
+    n <- readLn
+    passes <- input <$> readFile "input4.txt"
+    putStrLn . show $ part n passes
+    where part 1 = part1
+          part 2 = part2
+          part _ = const 0
+
