c783bbf6105a8215a8703f540a62a6acfc32ad4a
Thalia Nero <chrisvn00@gmail.com>
2020-12-06 15:59:22 -0500

    Puzzle 6

---
+++ 2020/Puzzle6.hs
@@ -0,0 +1,36 @@
+-- Thalia Nero, Advent of Code 2020
+module Puzzle6 where
+
+import AdventUtil
+import Data.List
+
+text1 :: String
+text1 = "abc\n\
+        \\n\
+        \a\n\
+        \b\n\
+        \c\n\
+        \\n\
+        \ab\n\
+        \ac\n\
+        \\n\
+        \a\n\
+        \a\n\
+        \a\n\
+        \a\n\
+        \\n\
+        \b"
+
+-- Parses the input
+input :: String -> [String]
+input = blocks " "
+
+-- Solves part 1.
+part1 :: [String] -> Int
+part1 xs = sum $ length . nub . filter (/= ' ') <$> xs
+
+part2 :: [String] -> Int
+part2 xs = sum $ length . foldl1 intersect . words <$> xs
+
+main :: IO ()
+main = putStrLn =<< show . part2 . input <$> readFile "input6.txt"
